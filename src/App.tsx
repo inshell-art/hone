20c5d71075fd5cb3c9f09257a8d74927697cc71d
Inshell <144088578+inshell-art@users.noreply.github.com>
2024-03-21 07:37:35 +0000

    Add basic editor

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import HoneEditor from "./components/HoneEditor";
+
+const App = () => {
+  return <HoneEditor />;
+};
+
+export default App;
---
+++ src/components/HoneEditor.tsx
@@ -0,0 +1,79 @@
+import { $getRoot, $getSelection, EditorState } from "lexical";
+import { useEffect } from "react";
+
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+import styles from "./HoneEditor.module.css";
+
+const theme = {
+  // Theme styling goes here
+  // ...
+};
+
+// When the editor changes, you can get notified via the
+// LexicalOnChangePlugin!
+const onChange =
+  () =>
+  (editorState: EditorState): void => {
+    editorState.read(() => {
+      // Read the contents of the EditorState here.
+      const root = $getRoot();
+      const selection = $getSelection();
+
+      console.log(root, selection);
+    });
+  };
+
+// Lexical React plugins are React components, which makes them
+// highly composable. Furthermore, you can lazy load plugins if
+// desired, so you don't pay the cost for plugins until you
+// actually use them.
+const MyCustomAutoFocusPlugin = () => {
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    // Focus the editor when the effect fires!
+    editor.focus();
+  }, [editor]);
+
+  return null;
+};
+
+// Catch any errors that occur during Lexical updates and log them
+// or throw them as needed. If you don't throw them, Lexical will
+// try to recover gracefully without losing user data.
+const onError = (error: Error) => {
+  console.error(error);
+};
+
+const HoneEditor = () => {
+  const initialConfig = {
+    namespace: "MyEditor",
+    theme,
+    onError,
+  };
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <PlainTextPlugin
+        contentEditable={
+          <div className={styles.editorContainer}>
+            <ContentEditable />
+          </div>
+        }
+        placeholder={<div>Enter some text...</div>}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <OnChangePlugin onChange={onChange} />
+      <HistoryPlugin />
+      <MyCustomAutoFocusPlugin />
+    </LexicalComposer>
+  );
+};
+
+export default HoneEditor;
