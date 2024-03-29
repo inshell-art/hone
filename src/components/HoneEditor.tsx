9cbed22fbf67ba7bc91da74a182fdc6d23f7993e
bigU <bigu@bigUs-MacBook-Pro.local>
2024-03-29 18:19:17 +0800

    Finish style for article title and facet title

--- src/components/HoneEditor.tsx
+++ src/components/HoneEditor.tsx
@@ -1,39 +1,17 @@
-import { $getRoot, $getSelection, EditorState } from "lexical";
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
-import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
+
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
-import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
 
 import HoneTheme from "../themes/HoneTheme";
-import StyleArticleTitlePlugin from "../plugins/StyleArticleTitlePlugin";
 import { HeadingNode } from "@lexical/rich-text";
-import { TextNode } from "lexical";
-
-// When the editor changes, you can get notified via the
-// LexicalOnChangePlugin!
-const onChange =
-  () =>
-  (editorState: EditorState): void => {
-    editorState.read(() => {
-      // Read the contents of the EditorState here.
-      const root = $getRoot();
-      const selection = $getSelection();
-
-      console.log(root, selection);
-    });
-  };
-
-// Lexical React plugins are React components, which makes them
-// highly composable. Furthermore, you can lazy load plugins if
-// desired, so you don't pay the cost for plugins until you
-// actually use them.
-
-// Catch any errors that occur during Lexical updates and log them
-// or throw them as needed. If you don't throw them, Lexical will
-// try to recover gracefully without losing user data.
+import { ParagraphNode, TextNode } from "lexical";
+import StyleArticleTitlePlugin from "../plugins/StyleArticleTitlePlugin";
+import TreeViewPlugin from "../plugins/TreeViewPlugin";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import StyleFacetTitlePlugin from "../plugins/StyleFacetTitlePlugin";
 
 const HoneEditor = () => {
   const initialConfig = {
@@ -42,25 +20,28 @@ const HoneEditor = () => {
     onError(error: Error) {
       throw error;
     },
-    nodes: [TextNode, HeadingNode],
+    nodes: [TextNode, HeadingNode, ParagraphNode],
   };
 
   const Placeholder = () => {
-    return <div className="editor-placeholder">Type your article here.</div>;
+    return (
+      <div className="editor-placeholder">Type your article title here...</div>
+    );
   };
 
   return (
     <LexicalComposer initialConfig={initialConfig}>
       <div className="editor-container">
-        <PlainTextPlugin
+        <RichTextPlugin
           contentEditable={<ContentEditable className="editor-input" />}
           placeholder={<Placeholder />}
           ErrorBoundary={LexicalErrorBoundary}
         />
         <StyleArticleTitlePlugin />
+        <StyleFacetTitlePlugin />
         <AutoFocusPlugin />
         <HistoryPlugin />
-        <OnChangePlugin onChange={onChange} />
+        <TreeViewPlugin />
       </div>
     </LexicalComposer>
   );
--- src/plugins/StyleFacetTitlePlugin.ts
+++ src/plugins/StyleFacetTitlePlugin.ts
@@ -3,23 +3,30 @@ import { $getRoot, ElementNode, TextNode } from "lexical";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { $createHeadingNode } from "@lexical/rich-text";
 
-const TransformFirstTextNodeParentPlugin = () => {
+const StyleFacetTitlePlugin = () => {
   const [editor] = useLexicalComposerContext();
 
   useEffect(() => {
     const removeNodeTransform = editor.registerNodeTransform(
       TextNode,
       (textNode) => {
+        // Ensure the text node starts with '$'
+        if (!textNode.getTextContent().startsWith("$")) {
+          return;
+        }
+
         const root = $getRoot();
         const firstChild = root.getFirstChild();
         const parent = textNode.getParent();
 
+        // Ensure the parent of the text node is not the first child of the root
+        // and not already styled as a heading
         if (
-          parent === firstChild &&
+          parent !== firstChild &&
           parent.getType() !== "heading" &&
           parent instanceof ElementNode
         ) {
-          const headingNode = $createHeadingNode("h1");
+          const headingNode = $createHeadingNode("h2");
 
           parent.getChildren().forEach((child) => {
             headingNode.append(child);
@@ -38,4 +45,4 @@ const TransformFirstTextNodeParentPlugin = () => {
   return null;
 };
 
-export default TransformFirstTextNodeParentPlugin;
+export default StyleFacetTitlePlugin;
