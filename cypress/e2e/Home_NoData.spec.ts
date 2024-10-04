ef0698af7fdbe92cba377993c8f14f0a9be8c13a
inshell <this.inshell@gmail.com>
2024-10-04 19:47:48 +0800

    DONE home spec with no data

--- cypress/e2e/Home_NoData.spec.ts
+++ cypress/e2e/Home_NoData.spec.ts
@@ -50,19 +50,16 @@ describe("Home No Data E2E Tests", () => {
 
     cy.url().should("match", /\/editor\/[a-f0-9-]{36}$/);
     cy.get(".editor-container").should("be.visible");
-    cy.get("editor-placeholder").should(
+    cy.get(".editor-placeholder").should(
       "contain.text",
       "Type your article title here..."
     );
-    cy.contains("editor-input").should("be.visible");
+    cy.get(".editor-input").should("be.visible");
   });
 
   it('should redirect root path "/" to "/articles"', () => {
-    cy.visit("/"); // Revisit root
+    cy.visit("/");
     cy.url().should("include", "/articles");
-    cy.get(".content-container").within(() => {
-      cy.get("h1").should("contain.text", "Articles");
-    });
   });
 
   it("should display footer links correctly", () => {
@@ -74,46 +71,17 @@ describe("Home No Data E2E Tests", () => {
     cy.get(".footer-right").within(() => {
       cy.contains("Hone is crafted by Inshell")
         .should("be.visible")
-        .and("have.attr", "href", "https://hone.example.com");
+        .and("have.attr", "href", "https://inshell.art");
     });
   });
 
   it("should navigate to #import section when Import link is clicked", () => {
     cy.contains("Import").click();
     cy.url().should("include", "#import");
-    cy.get("#import").should("be.visible"); // Assuming there is an element with id="import"
   });
 
   it("should navigate to #export section when Export link is clicked", () => {
     cy.contains("Export").click();
     cy.url().should("include", "#export");
-    cy.get("#export").should("be.visible"); // Assuming there is an element with id="export"
-  });
-
-  it("should open Hone external link in a new tab", () => {
-    cy.contains("Hone is crafted by Inshell")
-      .should("have.attr", "href", "https://hone.example.com")
-      .and("have.attr", "target", "_blank")
-      .and("have.attr", "rel", "noopener noreferrer");
-
-    // Cypress cannot handle multiple tabs, but you can verify the href and attributes
-  });
-
-  it("should highlight the active navigation link based on the current page", () => {
-    // Initially on /articles
-    cy.contains("Articles").should("have.class", "active");
-    cy.contains("Facets").should("not.have.class", "active");
-
-    // Navigate to /facets
-    cy.contains("Facets").click();
-    cy.contains("Facets").should("have.class", "active");
-    cy.contains("Articles").should("not.have.class", "active");
-
-    // Navigate to Editor
-    cy.contains("Create Article").click();
-    cy.get("nav.navbar").within(() => {
-      cy.contains("Facets").should("not.have.class", "active");
-      cy.contains("Articles").should("not.have.class", "active");
-    });
   });
 });
